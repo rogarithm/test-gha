e012ca45c62d53b2520461efa726dcb58b279074
sehun-kim <rogarithm@gmail.com>
2022-10-12 11:05:26 +0900

    [#7] push만 했을 때는 action이 실행되지 않아야 한다

--- src/main/java/com/example/gha/test/ci/MemberApp.java
+++ src/main/java/com/example/gha/test/ci/MemberApp.java
@@ -7,7 +7,7 @@ import com.example.gha.test.ci.member.MemberServiceImpl;
 
 public class MemberApp {
     public static void main(String[] args) {
-        System.out.println("hello!");
+        System.out.println("hello! again!");
         MemberService memberService = new MemberServiceImpl();
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
